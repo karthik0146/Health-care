30f6bc1ad095934b1ae0c2395dab238d94c01ecb
karthik0146 <byreddykartheek@gmail.com>
2025-08-05 15:09:10 +0530

    First Commit

---
+++ src/app/components/careunit/careunit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from '../sidebar/home.component';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-careunit',
+  imports: [HomeComponent],
+  templateUrl: './careunit.component.html',
+  styleUrl: './careunit.component.css'
+})
+export class CareunitComponent {
+
+}
---
+++ src/app/fluids/fluids.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from '../components/sidebar/home.component';
+
+@Component({
+  selector: 'app-fluids',
+  imports: [HomeComponent],
+  templateUrl: './fluids.component.html',
+  styleUrl: './fluids.component.css'
+})
+export class FluidsComponent {
+
+}
